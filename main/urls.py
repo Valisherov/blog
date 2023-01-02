8080d215d430f4a58d9b1510220249ab871fe882
Your Name <you@example.com>
2023-01-02 03:48:57 -0800

    'first'

---
+++ main/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import *
+
+urlpatterns=[
+    path('',Home),
+    path('resume/',Resume),
+    path('contact/',Contact),
+    path('send_msg/',Send),
+    path('blog/',Blog),
+    path('views/<int:pk>',BlogDetail.as_view()),
+    path('comment/',Commenting)
+]
+
+
